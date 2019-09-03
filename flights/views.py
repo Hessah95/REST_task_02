45640727276282092c7208fcdea13598732345e2
Hessah95 <hessah.alhamadi.95@gmail.com>
2019-09-03 10:30:43 +0300

    Fist Commit

--- flights/views.py
+++ flights/views.py
@@ -1,8 +1,15 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.generics import RetrieveAPIView
 from datetime import datetime
+from rest_framework.generics import (
+	DestroyAPIView, RetrieveUpdateAPIView
+	)
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer
+from .serializers import (
+	FlightSerializer, BookingSerializer, 
+	DetailSerializer, UpdateSerializer
+	)
 
 
 class FlightsList(ListAPIView):
@@ -15,3 +22,20 @@ class BookingsList(ListAPIView):
 	serializer_class = BookingSerializer
 
 
+class DetailView(RetrieveAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = DetailSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'object_id'
+
+class UpdateView(RetrieveUpdateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = UpdateSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'object_id'
+
+class DeleteView(DestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = FlightSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'object_id'
